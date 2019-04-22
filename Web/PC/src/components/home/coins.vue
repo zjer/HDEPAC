e63c21370861e6df58fd88e59fcac600fd6f0ee2
shijun <15651026970@163.com>
2019-04-22 20:27:52 +0800

    页面调整

--- Web/PC/src/components/home/coins.vue
+++ Web/PC/src/components/home/coins.vue
@@ -16,14 +16,17 @@
           </template>
         </el-table-column>
         <el-table-column prop="price" :label="$t('message.price')">
-        </el-table-column>
-        <el-table-column prop="change" :label="$t('message.change24H')">
+          <template slot-scope="scope">
+            <div class="up" v-bind:class="{down: scope.row.isDown}">{{ scope.row.price }}</div>
+          </template>
         </el-table-column>
         <el-table-column prop="trade_24h" :label="$t('message.trade24H')">
         </el-table-column>
         <el-table-column prop="market_cap" :label="$t('message.cap')">
         </el-table-column>
         <el-table-column prop="supply" :label="$t('message.supply')">
+        </el-table-column>
+        <el-table-column prop="change" :label="$t('message.change24H')">
         </el-table-column>
       </el-table>
     </div>
@@ -40,25 +43,51 @@
       return {
         isloading: false,
         curIndex: -1,
-        tableData: []
+        tableData: [],
+        isDown: true,
+        oldPrice: [],
+        nowPrice: [],
       }
     },
     created() {
       this.getCoinPrice();
+      this.getNowPrice();
     },
     mounted() {
       if (this.timer) {
         clearInterval(this.timer);
       } else {
         this.timer = setInterval(() => {
-          this.getCoinPrice();
-        }, 300000);
+          this.getNowPrice();
+        }, 30000);
       }
     },
     destroyed() {
       clearInterval(this.timer);
     },
     methods: {
+      getNowPrice() {
+        axios.get(baseUrl + '/httpclient/getNowPrice').then(res => {
+          if (res.data.state) {
+            let coins = res.data.rows.data.coin_rate;
+            let legals = res.data.rows.data.legal_rate;
+            this.$set(this.oldPrice, 0, this.nowPrice);
+            this.nowPrice = [];
+            for (let legal in legals) {
+              let msg = {};
+              msg[legal] = legals[legal];
+              this.nowPrice.push({
+                name: legal,
+                price: legals[legal]
+              });
+            }
+            this.nowPrice.push({name: 'BTC', price: coins.BTC});
+            this.nowPrice.push({name: 'BTC', price: coins.ETH});
+            console.log(this.oldPrice);
+            console.log(this.nowPrice);
+          }
+        });
+      },
       getCoinPrice() {
         axios.get(baseUrl + '/httpclient/getCoinPrice').then(res => {
           this.isloading = true;
@@ -67,7 +96,18 @@
             let datas = res.data.rows.data.data;
             console.log(datas);
             datas.forEach(ele => {
-              ele.change = ele.change > 0 ? '+' +ele.change + '%' : ele.change + '%';
+              ele.supply = ele[10];
+              this.nowPrice.forEach((item, index) => {
+                console.log(item.name);
+                console.log(ele[1]);
+                if (item.name === ele[1]) {
+                  console.log(item.name);
+                  console.log(ele[1]);
+                  ele.price = item.price * 6.7;
+                  ele.isDown = parseFloat(item.price) < this.oldPrice[index] ? true : false;
+                }
+              });
+              ele.change = ele[13] > 0 ? '+' + ele[13] + '%' : ele[13] + '%';
               this.tableData.push(ele);
               this.isloading = false;
             });
